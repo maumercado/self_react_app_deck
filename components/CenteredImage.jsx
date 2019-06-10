2358e3bc4314fb3e873d8155c917b7ae9d8ef732
Mauricio Mercado <mau@maumercado.com>
2019-06-09 21:33:55 -0700

    fix images on build

---
+++ components/CenteredImage.jsx
@@ -0,0 +1,11 @@
+import { Image } from 'mdx-deck';
+import styled from 'styled-components';
+
+
+const CenteredImage = styled(Image)`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default CenteredImage;
